70bd5016e975bf88e5693eae2b2c86df395e1fd3
Anayatc <anayatc@sky.com>
2017-04-20 21:37:11 +0100

    introducing variables II

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = "Anayat Chowdhury"
+my_age = 27  # not a lie
+my_height = 173  # in cm this is england not america
+my_weight = 80  # in kg same reason as above
+my_eyes = "Brown"
+my_teeth = "White"
+my_hair = "Brown"
+
+print("Lets talk about %s." % my_name)
+print("He's %d centimeters tall." % my_height)
+print("He's %d kilograms heavy." % my_weight)
+print("Actually thar's pretty heavy.")
+print("He's got %s eyes and %s hair." %(my_eyes, my_hair))
+print("His teeth are usually %s depending on the coffee" % my_teeth)  # damn you starbucks!!!
+
+
+# this line is tricky, try to gt it exactly right
+print("If I add %d, %d, and %d I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight))
