79e22bdcc154a28d65148d3fc7bd1cab02e82afe
Anayatc <anayatc@sky.com>
2017-04-20 20:40:20 +0100

    printing and commenting out code

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# A comment, this so you can read your program later.
+# Anything after the # is ignored by python.
+
+print('I could have code like this.') # and the comment after is ignored
+
+#you can use a comment to "disable" or comment out a piece of code:
+#print('This wont run.')
+
+print('This will run.')
+
